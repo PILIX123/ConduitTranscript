143dc55a0b2434cc0a3235a4c73f3d8469b7223c
Pierre-Luc Robitaille <2012805@cegeplevis.ca>
2024-03-26 13:20:50 -0400

    p

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ podcast = Podcast(get("https://www.relay.fm/conduit/feed").content)
 
 episodeUrls = []
 for episode in podcast.items:
-    episodeUrls.append(episode.enclosure_url)
+    episodeUrls.append((episode.title, episode.enclosure_url))
 
+t = episodeUrls[0][0]
+with open(f"{t}.txt", "w") as file:
+    file.write("text")
 model = whisper.load_model("large", device="cuda:0")
-result = model.transcribe(episodeUrls[0])
-print(result["text"])
+result = model.transcribe(episodeUrls[0][1])
