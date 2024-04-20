3d77ad5328887fa23ba4186869725c531be92deb
Pierre-Luc Robitaille <2012805@cegeplevis.ca>
2024-04-20 04:01:48 -0400

    blurp

---
+++ biWeeklyConduit.py
@@ -0,0 +1,14 @@
+import whisper
+from pyPodcastParser.Podcast import Podcast
+from requests import get
+
+
+conduit = Podcast(get("https://www.relay.fm/conduit/feed").content)
+latestEpisode = conduit.items[0]
+newTitle = latestEpisode.title.replace(":", "")
+newTitle = newTitle.replace("?", "")
+episodeData = (newTitle, latestEpisode.enclosure_url)
+model = whisper.load_model("large", "cuda:0")
+result = model.transcribe(episodeData[1])
+with open(f"{episodeData[0]}.txt", "w") as f:
+    f.write(result["text"])
