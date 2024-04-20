9b7db7e4e20cde479d0388b5f47051e6c6b4290a
Pierre-Luc Robitaille <2012805@cegeplevis.ca>
2024-04-20 04:02:13 -0400

    blurpblurp

--- biWeeklyConduit.py
+++ biWeeklyConduit.py
@@ -10,5 +10,5 @@ newTitle = newTitle.replace("?", "")
 episodeData = (newTitle, latestEpisode.enclosure_url)
 model = whisper.load_model("large", "cuda:0")
 result = model.transcribe(episodeData[1])
-with open(f"{episodeData[0]}.txt", "w") as f:
+with open(f"{episodeData[0]}.mp3.txt", "w") as f:
     f.write(result["text"])
